d531218ba21fe520ae3a52cc32ea07ff17272ca9
AndreyPokotilo <dpon444gmail.com>
2024-08-02 22:03:15 +0200

    fixed cors

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ const authRouter = require("./routes/api/auth");
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-
-app.use(logger(formatsLogger));
 app.use(cors({
   origin: process.env.CLIENT_URL,
   credentials: true,
 }));
+app.use(logger(formatsLogger));
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
